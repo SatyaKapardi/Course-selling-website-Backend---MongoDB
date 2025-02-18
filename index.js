8715641a62eac0413fdd7b109c8878adfe7d6a08
Srisatya Kapardi Budi <94280881+SatyaKapardi@users.noreply.github.com>
2025-02-18 22:33:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const app = express();
 const adminRouter = require("./routes/admin")
 const userRouter = require("./routes/user");
+const { initialiseLocker } = require('./db/index.js');
 
 // Middleware for parsing request bodies
 app.use(bodyParser.json());
@@ -10,6 +11,11 @@ app.use("/admin", adminRouter)
 app.use("/user", userRouter)
 
 const PORT = 3000;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+
+initialiseLocker().then(()=>{
+
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    });
+})
+
